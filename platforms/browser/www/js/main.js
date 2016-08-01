5e8be8f0e12f72b09caa52f1ba9ac16f75b6bf81
axy7779 <axy7779@sina.com>
2016-08-01 12:48:01 +0800

    v1

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded',function(){
+    var swiper = new Swiper('.swiper-container',{
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop:true,
+        autoplay:1000,
+        autoplayDisableOnInteraction:true
+    });
+},false);
+
